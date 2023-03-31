cba1899c8524e73730d2e2fbc96aae1f598d5ad3
MikePlekan <70487027+MikePlekan@users.noreply.github.com>
2023-03-31 05:26:06 +0000

    added useful website

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::io;
 
 //https://code.visualstudio.com/docs/languages/rust
+//https://doc.rust-lang.org/rust-by-example/
 use io::prelude::*;
 
 fn main() -> io::Result<()> {
     let mut buffer = [0; 100];
     let mut f =io::stdin();
-    
-
     
     //wait for input until enter is pressed
     let n = f.read(&mut buffer)?;
