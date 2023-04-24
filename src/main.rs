7a240bb08fdce6da283468c5cb528601d20da9db
MikePlekan <70487027+MikePlekan@users.noreply.github.com>
2023-04-24 17:59:27 +0000

    change

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,30 @@
 use std::io;
+use rust_decimal::prelude::*;
 
 //https://code.visualstudio.com/docs/languages/rust
 //https://doc.rust-lang.org/rust-by-example/
 //https://youtu.be/uKlHwko36c4 make libary for many langs
-//https://www.tutorialspoint.com/rust/index.htm
-use io::prelude::*;
-use std::thread;
-use std::time::Duration;
-use std::sync::{Mutex, Arc};
+//https://www.tutorialspoint.com/rust/index.html
 
 fn main() -> io::Result<()> {
-    /* let mut buffer = [0; 100];
-    let mut f =io::stdin();
-    let 
-    //wait for input until enter is pressed
-    let n = f.read(&mut buffer)?;
-    println!("The bytes: {:?}", &buffer[..n]);
-    println!("The string: {}", String::from_utf8_lossy(&buffer[..n])); */
-
-    let handles=vec![
-        thread::spawn(||{
-            for i in 1..10{
-                println!("hi number {} from the spawned thread!",i);
-                thread::sleep(Duration::from_millis(1));
-            }
-        }),
-        thread::spawn(||{
-            for i in 101..200{
-                println!("hi number {} from the spawned thread!",i);
-                thread::sleep(Duration::from_millis(1));
-            }
-        }),
-    ];
-    for handle in handles{
-        handle.join().unwrap();
-    }
-    println!("{}",checkeven(4));
+    let now = std::time::Instant::now();
+    let pi = pi(10000000);
+    let elapsed = now.elapsed();
+    println!("pi is {} and it took {} seconds", pi, elapsed.as_secs_f64());
     Ok(())
 }
-fn checkeven(n: i32) -> String {
-    if n & 1 == 0 {
-        return String::from("True");
-    } else {
-        return String::from("False");
+//Gregory–Leibniz series
+fn pi(n: u64) -> Decimal {
+    let mut pi = Decimal::new(0, 0);
+    let mut k = Decimal::new(0, 0);
+    let one = Decimal::new(1, 0);
+    let two = Decimal::new(2, 0);
+    let four = Decimal::new(4, 0);
+    let mut sign = one;
+    for _ in 0..n {
+        pi = pi + sign * (four / (two * k + one));
+        k = k + one;
+        sign = sign * -one;
     }
+    pi
 }
